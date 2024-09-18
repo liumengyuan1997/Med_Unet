67e49008d579d19e788919e10aab140574bfee69
liumengyuan1997 <liumengyuan23@gmail.com>
2024-09-17 18:24:20 -0700

    feat(3d reconstruction)

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ from albumentations.pytorch import ToTensorV2
 
 dir_img = Path('./data/01s1_original/imgs/axial')
 dir_mask = Path('./data/01s1_original/masks/axial')
-dir_checkpoint = Path('./checkpoints/')
+dir_checkpoint = Path('./checkpoints/axial')
 
 
 def train_model(
